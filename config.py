15e5c0afa115af2281e11fd9ce54a4ce79c14758
dongzefan20242024 <dongzefan@cvloo.com>
2024-12-07 19:20:47 -0500

    conig

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    SECRET_KEY = "default_secret_key"  # 可替换为你自己的
+    PINECONE_API_KEY = "pcsk_5U9NYr_JDQVstqgVePxuAEBMCrKLt8CbdeRdv9aGBrFGiEbV5XEWCnDSw5DzwVLPfpJNYk"
+    PINECONE_ENV = "us-east-1"  # 使用你的环境名称
+    DATABASE_URI = 'data/sample.db'  # 数据库路径
